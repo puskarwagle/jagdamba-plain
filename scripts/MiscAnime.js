14be3e04943038738ba642ac4941e5f197801070
puskarwagle <puskarwagle17@gmail.com>
2022-11-01 22:10:55 +0545

    some changes

--- scripts/MiscAnime.js
+++ scripts/MiscAnime.js
@@ -1,26 +1,3 @@
-//Header
-const handleHeader = () => {
-  const headerWhite = document.querySelector('.HeadBangHi');
-  const page1 = document.querySelector(".FrontText");
-  
-  let page1Top;
-  
-  const update = () => {
-    page1Top = page1.getBoundingClientRect().top
-    if (page1Top < 153) {
-            headerWhite.classList.add("HeadBangSh");
-    }
-    if (page1Top > 153) {
-            headerWhite.classList.remove("HeadBangSh");
-    }
-    requestAnimationFrame(update)
-  }
- // console.log(page1.getBoundingClientRect().top)
-  update()
-}
-
-handleHeader();
-
 
 //Fronttext 1 and 2 animations
 const textanime = new IntersectionObserver((entries) => {
@@ -48,14 +25,4 @@ const formheader = document.querySelectorAll(".FormHead");
 formheader.forEach((el) => formhead.observe(el));
 
 
-//Rectangle boxes animation
-const rectboxes = new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-				//console.log(entry)
-				if (entry.isIntersecting) {
-						entry.target.classList.add("grid-boxAnime");
-				}
-		});
-});
-const rectbox = document.querySelectorAll(".grid-box");
-rectbox.forEach((el) => rectboxes.observe(el));
+
--- scripts/RectBox.js
+++ scripts/RectBox.js
@@ -1,1 +1,28 @@
+//Rectangle boxes Intersection Observer animation
+const rectboxes = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+				//console.log(entry)
+				if (entry.isIntersecting) {
+						entry.target.classList.add("grid-boxAnime");
+				}
+		});
+});
+const rectbox = document.querySelectorAll(".grid-box");
+rectbox.forEach((el) => rectboxes.observe(el));
+
+//Rectangle Grid boxes and pTexts
+$(function() {
+    $(".One").click(function() {
+    		console.log("function run!")
+        $(".pOne").slideDown(1000);
+    });
+    $(".Two").click(function() {
+    		console.log("function run!")
+        $(".pTwo").slideDown(1000);
+    });
+    $(".Three").click(function() {
+    		console.log("function run!")
+        $(".pThree").slideDown(1000);
+    });
+});
 
